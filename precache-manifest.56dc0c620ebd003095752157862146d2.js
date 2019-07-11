7f1897a247cc4069dc07e2f9f5857a98feff8435
Kevin <kevin.freistroffer@gmail.com>
2019-07-11 06:41:03 -0700

    first commit

---
+++ precache-manifest.56dc0c620ebd003095752157862146d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ee343ef06b1813118a98ee03f5f00b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c802d526cd21f4e2261",
+    "url": "/static/css/main.30b339ef.chunk.css"
+  },
+  {
+    "revision": "194044371f5392619d3e",
+    "url": "/static/js/2.470ab509.chunk.js"
+  },
+  {
+    "revision": "1c802d526cd21f4e2261",
+    "url": "/static/js/main.a776e4c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
