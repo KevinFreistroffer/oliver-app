e24a3dc4c7b06b485d58aeec9a6cbce9b363cd6e
Kevin <kevin.freistroffer@gmail.com>
2019-07-11 12:15:48 -0700

    path added to sendFile

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './')));
 
 app.use('*', (req, res) => {
-	res.sendFile('./index.html');
+	res.sendFile(path.join(__dirname, './index.html');
 })
 
 // catch 404 and forward to error handler
